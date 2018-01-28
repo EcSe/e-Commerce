84dbef7611bd1d8d39ae3989d5304eb0bfa6d6c4
DESKTOP-57BOK1J\Elvin <ecse88@gmail.com>
2018-01-28 15:38:41 -0500

    Resolviendo el problema del maxReceiveBuffering en los servicios de producto

--- AppEcommerce/CapaServicios/App_Code/ServiceProducto.cs
+++ AppEcommerce/CapaServicios/App_Code/ServiceProducto.cs
@@ -5,23 +5,38 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
+using CapaEntidades;
+using System.Data;
+using CapaNegocio;
 // NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de clase "ServiceProducto" en el código, en svc y en el archivo de configuración a la vez.
 public class ServiceProducto : IServiceProducto
 {
-	
+    ProductoBL productoBL = new ProductoBL();
 
     public System.Data.DataSet Listar()
     {
-        throw new NotImplementedException();
+        return productoBL.Listar();
     }
 
     public List<string> Agregar(CapaEntidades.ProductoEntidad producto)
     {
-        throw new NotImplementedException();
+        List<String> Resultados = new List<String>();
+
+        String Estado = Convert.ToString(productoBL.Agregar(producto));
+        String Mensaje = productoBL.Mensaje;
+        Resultados.Add(Estado);
+        Resultados.Add(Mensaje);
+        return Resultados;
     }
 
     public List<string> Eliminar(string codProd)
     {
-        throw new NotImplementedException();
+        List<String> Resultados = new List<String>();
+
+        String Estado = Convert.ToString(productoBL.Eliminar(codProd));
+        String Mensaje = productoBL.Mensaje;
+        Resultados.Add(Estado);
+        Resultados.Add(Mensaje);
+        return Resultados;
     }
 }
